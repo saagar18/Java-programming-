ae840ee4dd2a99aa28e52047a770591d0e54e185
saagar <154490940+saagar18@users.noreply.github.com>
2024-07-13 09:06:50 +0530

    Create 41. Square and cube of decimal.java

---
+++ 41. Square and cube of decimal.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double number = 0.6; 
+        double square = number * number;
+        double cube = number * number * number;
+        System.out.println("Square Number: " + square);
+        System.out.println("Cube Number: " + cube);
+    }
+}

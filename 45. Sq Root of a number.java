f6372a8ead4673cbdfee9d23c5e34d4f1349a366
saagar <154490940+saagar18@users.noreply.github.com>
2024-07-13 09:09:30 +0530

    Create 45. Sq Root of a number.java

---
+++ 45. Sq Root of a number.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 6561; 
+        int sqrt = (int) Math.sqrt(number);
+        System.out.println("Square Root: " + sqrt + ", " + (-sqrt));
+    }
+}

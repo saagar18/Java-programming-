9018c4a8e5e7586b878788b3757208d397c1bbbb
saagar <154490940+saagar18@users.noreply.github.com>
2024-07-17 09:34:25 +0530

    Create 51. Multiplication Table.java

---
+++ 51. Multiplication Table.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int m = 4; 
+        int n = 10; 
+        for (int i = 1; i <= n; i++) {
+            int result = i * m;
+            System.out.println(i + "x" + m + "=" + result);
+        }
+    }
+}
